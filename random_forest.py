bc5c49167a9e2188f98787f489a0ee939a201ac4
Gabriel Freedman <gfreedman2108@comcast.net>
2018-11-11 22:36:20 -0500

    Initial commit, add data files

---
+++ random_forest.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 11 19:49:07 2018
+
+@author: Gabe Freedman
+"""
+
+# Change matplotlib back-end for XMing capability
+import matplotlib
+# matplotlib.use('TkAgg')
+
+####### GLOBAL IMPORTS #######
+import sys
+if sys.version_info[0] == 3:
+    import _pickle as cPickle
+else:
+    import cPickle
+
+
+import _pickle as cPickle
+# import moby2
+# from moby2.instruments import actpol
+import numpy as np
+from matplotlib import pyplot as plt
+
+####### GLOBAL VARIABLES #######
+DEPOT = '/data/actpol/depot'
+
+
+# import train and test TOD lists
+train_list = np.loadtxt(r'./data/2016_ar3_train.txt', dtype=str)
+test_list = np.loadtxt(r'./data/2016_ar3_test.txt', dtype=str)
+
+# Load pickle file contents
+with open(r'./data/mr3_pa2_s16_results.pickle', 'rb') as f:
+    data = cPickle.load(f, encoding='latin1')
