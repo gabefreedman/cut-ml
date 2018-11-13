ea4cea93bbf1eff0e6076f02dc16fa88f6ef8ea9
Gabriel Freedman <gfreedman2108@comcast.net>
2018-11-12 19:43:25 -0500

    Add random forest core

--- random_forest.py
+++ random_forest.py
@@ -21,7 +21,13 @@ else:
 # import moby2
 # from moby2.instruments import actpol
 import numpy as np
+import random
+import pandas as pd
 from matplotlib import pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 ##############################
 
 ####### GLOBAL VARIABLES #######
@@ -37,3 +43,82 @@ test_list = np.loadtxt(r'./data/2016_ar3_test.txt', dtype=str)
 # Load pickle file contents
 with open(r'./data/mr3_pa2_s16_results.pickle', 'rb') as f:
     data = cPickle.load(f, encoding='latin1')
+
+def generate_test_train_tods(tod_names):
+    
+    subset = random.sample(tod_names, 100)
+    
+    random.shuffle(subset)
+    
+    train_list = subset[:80]
+    test_list = subset[80:]
+    return train_list, test_list
+
+train_list, test_list = generate_test_train_tods(data['name'])
+
+# Generate list of indices corresponding to train/test TODs
+train_ind = [data['name'].index(tod) for tod in train_list]
+test_ind = [data['name'].index(tod) for tod in test_list]
+
+pckl_params = ['jumpDark', 'corrLive', 'rmsLive', 'kurtLive', 
+               'normDark', 'skewLive', 'DELive', 'jumpLive', 'gainDark', 'corrDark', 'sel']
+
+small_dct = {param: data[param] for param in pckl_params}
+
+def make_dfs(d, tod_list, tod_ind):
+    
+    df_list = []
+    
+    for i in range(len(tod_list)):
+        td = {k:d[k][:,tod_ind[i]] for k in d.keys()}
+        df_list.append(pd.DataFrame(td))
+    
+    df = pd.concat(df_list)
+    return df
+
+train_df = make_dfs(small_dct, train_list, train_ind)
+test_df = make_dfs(small_dct, test_list, test_ind)
+
+
+def split_features_labels(train,test):
+    
+    x_train = train.drop('sel', axis=1)
+    x_test = test.drop('sel', axis=1)
+    
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.fit_transform(x_test)
+    
+    y_train = np.array(train['sel'])
+    y_test = np.array(test['sel'])
+    
+    return x_train, x_test, y_train, y_test
+
+x_train, x_test, y_train, y_test = split_features_labels(train_df, test_df)
+
+
+regressor = RandomForestClassifier(n_estimators=50, random_state=0)  
+regressor.fit(x_train, y_train)  
+y_pred = regressor.predict(x_test)
+
+tp=0
+fp=0
+fn=0
+tn=0
+for i in range(len(y_pred)):
+    if y_test[i]==y_pred[i]==1:
+           tp += 1
+    if y_pred[i]==1 and y_test[i]!=y_pred[i]:
+           fp += 1
+    if y_test[i]==y_pred[i]==0:
+           tn += 1
+    if y_pred[i]==0 and y_test[i]!=y_pred[i]:
+           fn += 1
+           
+total = tp+fp+fn+tn
+print('True Positive: ' +str(tp) + ', ' + str(tp/total))
+print('False Positive: ' +str(fp) + ', ' + str(fp/total))
+print('False Negative: ' +str(fn) + ', ' + str(fn/total))
+print('True Negative: ' +str(tn) + ', ' + str(tn/total))
+print(classification_report(y_test,y_pred))  
+print(accuracy_score(y_test, y_pred))
